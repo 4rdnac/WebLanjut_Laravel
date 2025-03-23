40628c981c4f2b174066bca94d907b870b773411
4rdnac <candraahmaddani04@gmail.com>
2025-03-23 11:49:43 +0700

    P6 Praktikum 1

--- PWL_2025/week6/PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_2025/week6/PWL_POS/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ use App\Models\LevelModel;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
@@ -65,7 +66,12 @@ class UserController extends Controller
 
         return view('user.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
+    public function create_ajax()
+    {
+        $level = LevelModel::select('level_id', 'level_nama')->get();
 
+        return view('user.create_ajax')->with('level', $level);
+    }
     public function store(Request $request)
     {
         $request->validate([
@@ -84,6 +90,36 @@ class UserController extends Controller
 
         return redirect('/user')->with('success', 'User berhasil ditambahkan');
     }
+    public function store_ajax(Request $request)
+    {
+        // cek apakah request berupa ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|string|min:3|unique:m_user,username',
+                'nama' => 'required|string|max:100',
+                'password' => 'required|min:6'
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors(),
+                ]);
+            }
+
+            UserModel::create($request->all());
+            return response()->json([
+                'status' => true,
+                'message' => 'Data user berhasil disimpan'
+            ]);
+        }
+        redirect('/');
+    }
+
     public function show(string $id)
     {
         $user = UserModel::with('level')->find($id);
