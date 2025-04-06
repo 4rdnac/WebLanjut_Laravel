2b1a018e49f4b965aba0a868b7de85a7b54b6377
4rdnac <candraahmaddani04@gmail.com>
2025-04-06 17:40:36 +0700

    P7 Praktikum 3

--- PWL_2025/week7/PWL_POS/app/Models/UserModel.php
+++ PWL_2025/week7/PWL_POS/app/Models/UserModel.php
@@ -14,21 +14,31 @@ class UserModel extends Authenticatable
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model
     protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
 
-    protected $fillable = ['level_id','username','nama','password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
     protected $hidden = ['password']; // jangan ditampilkan saat di select
     protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
-    public function level(): BelongsTo{
-        return $this->belongsTo(LevelModel::class, 'level_id','level_id');
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
     // Mendapatkan nama role
-    public function getRoleName(): string{
+    public function getRoleName(): string
+    {
         return $this->level->level_nama;
     }
 
-    public function hasRole($role): bool {
+    // cek apakah user memiliki role tertentu
+    public function hasRole($role): bool
+    {
         return $this->level->level_kode == $role;
+    }
+
+    // mendapatkan kode role
+    public function getRole()
+    {
+        return $this->level->level_kode;
     }
 }
